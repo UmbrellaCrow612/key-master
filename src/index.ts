ed7968420089e80577a2d4c303aa294bcd18a041
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-03 17:38:21 +0000

    basic w3 spec added

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,60 @@
+import {
+  Alphabet,
+  AudioKey,
+  ControlCharacter,
+  DeviceKey,
+  EditingKey,
+  FunctionKey,
+  GlyphModifierKey,
+  IMEKey,
+  ModifierKey,
+  MultimediaKey,
+  MultimediaNumpadKey,
+  NavigationKey,
+  SpecialKey,
+  SpeechKey,
+  UIKey,
+  WhitespaceKey,
+} from "./w3";
+
 /**
- * Helper used to get custom data to pass to your callback when it's conditions are met
+ * Helper used to get custom data to pass to your callback when its conditions are met
  */
-type getDataCallback = (() => any | Promise<any>) | null;
+type GetDataCallback = (() => any | Promise<any>) | null;
 
 /**
  * The key value of a keydown can be mapped from W3C UI Events / DOM spec
+ *
+ * these are what a keydown / keyup `key` value can be and is what you can pass to listen to specific combinations to trigger logic
  */
-type PressableKey = "";
+type PressableKey =
+  | Alphabet
+  | ControlCharacter
+  | GlyphModifierKey
+  | SpecialKey
+  | ModifierKey
+  | WhitespaceKey
+  | NavigationKey
+  | EditingKey
+  | UIKey
+  | DeviceKey
+  | IMEKey
+  | FunctionKey
+  | MultimediaKey
+  | MultimediaNumpadKey
+  | AudioKey
+  | SpeechKey;
 
 /**
- * Custom callback to run when the specific keys registred are met
+ * Custom callback to run when the specific keys registered are met
  */
-type keyCallback = () => any | Promise<any>;
+type KeyCallback = (...args: any[]) => any | Promise<any>;
 
 /**
- * Represents a key combination master that holds are callbacks to run for specific key combinations are met
+ * Represents a key combination manager that holds callbacks to run for specific key combinations
  */
 export class KeyMaster {
-  private _getData: getDataCallback = null;
+  private _getData: GetDataCallback = null;
 
   /**
    * Holds a list of current keys being pressed down
@@ -27,65 +64,76 @@ export class KeyMaster {
   /**
    * Contains all the callbacks to run
    */
-  private readonly _callbacks: Map<string, keyCallback[]> = new Map();
+  private readonly _callbacks: Map<string, KeyCallback[]> = new Map();
 
-  /**
-   * Creates a key for list of pressable keys
-   * @param keys The list of keys to make a ID and then run specific callbacks for it
-   * @returns Key
-   */
-  private makeKey(keys: string[]) {
-    return keys.join("_");
-  }
-
-  constructor(getDataFunc: getDataCallback = null) {
+  constructor(getDataFunc: GetDataCallback = null) {
     this._getData = getDataFunc;
 
     document.addEventListener("keydown", this.handleKeyDown);
     document.addEventListener("keyup", this.handleKeyUp);
   }
 
-  private handleKeyDown(event: KeyboardEvent) {
+  /**
+   * Creates a key for list of pressable keys
+   * @param keys The list of keys to make an ID for specific callbacks
+   * @returns Key
+   */
+  private makeKey(keys: string[]): string {
+    return keys.sort().join("_");
+  }
+
+  private handleKeyDown = (event: KeyboardEvent) => {
     this._keys.add(event.key);
 
-    let pressed = Array.from(this._keys.values());
-    let key = this.makeKey(pressed);
+    const pressed = Array.from(this._keys.values());
+    const key = this.makeKey(pressed);
 
-    let callbacks = this._callbacks.get(key);
+    const callbacks = this._callbacks.get(key);
     if (callbacks) {
       this.run(callbacks);
     }
-  }
+  };
 
-  private async run(callbacks: keyCallback[]) {
-    for (let c of callbacks) {
-      await c();
+  private async run(callbacks: KeyCallback[]): Promise<void> {
+    const data = this._getData ? await this._getData() : null;
+    for (const callback of callbacks) {
+      await callback(data);
     }
   }
 
-  private handleKeyUp(event: KeyboardEvent) {
+  private handleKeyUp = (event: KeyboardEvent) => {
     this._keys.delete(event.key);
+  };
+
+  add(targetKeys: PressableKey[], callback: KeyCallback): void {
+    const key = this.makeKey(targetKeys);
+
+    if (!this._callbacks.has(key)) {
+      this._callbacks.set(key, []);
+    }
+
+    this._callbacks.get(key)!.push(callback);
   }
 
-  add(targetKeys: PressableKey[], callback: keyCallback) {
-    let key = this.makeKey(targetKeys)
-
-    this._callbacks.get(key)?.push(callback)
-  }
-
-  remove(callback: keyCallback) {
-    let callbacks = this._callbacks.values();
-
-    for (let cb of callbacks) {
-      // remove in place
+  remove(callback: KeyCallback): void {
+    for (const [key, callbacks] of this._callbacks.entries()) {
+      const index = callbacks.indexOf(callback);
+      if (index !== -1) {
+        callbacks.splice(index, 1);
+        if (callbacks.length === 0) {
+          this._callbacks.delete(key);
+        }
+      }
     }
   }
 
   /**
-   * Call at the end beofre destryoing it to clean
+   * Call before destroying to clean up event listeners
    */
-  dispose() {
+  dispose(): void {
     document.removeEventListener("keydown", this.handleKeyDown);
     document.removeEventListener("keyup", this.handleKeyUp);
+    this._callbacks.clear();
+    this._keys.clear();
   }
 }
